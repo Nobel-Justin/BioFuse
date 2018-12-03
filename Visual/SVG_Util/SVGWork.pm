798a3c0243271be9475323bb5c3a18820029ed60
Nobel-Justin <wenlongkxm@gmail.com>
2018-12-04 01:00:40 +0800

    add SVGWork.pm

---
+++ Visual/SVG_Util/SVGWork.pm
@@ -0,0 +1,66 @@
+package BioFuse::Visual::SVG_Util::SVGWork;
+
+use strict;
+use warnings;
+use SVG;
+require Exporter;
+
+#----- systemic variables -----
+our (@ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+my ($VERSION, $DATE, $AUTHOR, $EMAIL, $MODULE_NAME);
+@ISA = qw(Exporter);
+@EXPORT = qw/
+              initialize_SVG_obj
+              output_SVG_file
+            /;
+@EXPORT_OK = qw();
+%EXPORT_TAGS = ( DEFAULT => [qw()],
+                 OTHER   => [qw()]);
+
+$MODULE_NAME = 'BioFuse::Visual::SVG_Util::SVGWork';
+#----- version --------
+$VERSION = "0.01";
+$DATE = '2018-12-04';
+
+#----- author -----
+$AUTHOR = 'Wenlong Jia';
+$EMAIL = 'wenlongkxm@gmail.com';
+
+#--------- functions in this pm --------#
+my @functoion_list = qw/
+                        initialize_SVG_obj
+                        output_SVG_file
+                     /;
+
+#--- initilize SVG object ---
+sub initialize_SVG_obj{
+    # options
+    shift if (@_ && $_[0] =~ /$MODULE_NAME/);
+    my %parm = @_;
+    my $bg_width  = $parm{bg_width} || '100';
+    my $bg_height = $parm{bg_height} || '100';
+    my $bg_col = $parm{bg_col} || 'white';
+    my $bg_stroke_col = $parm{bg_stroke_col} || 'none';
+    my $AUTHOR = $parm{AUTHOR} || 'test';
+    my $EMAIL = $parm{EMAIL} || 'test@test.com';
+
+    my $svg_obj = SVG->new( width=>$bg_width, height=>$bg_height, author=>$AUTHOR, 'author-mail'=>$EMAIL);
+    $svg_obj->rect(x=>0, y=>0, width=>$bg_width, height=>$bg_height, fill=>$bg_col, stroke=>$bg_stroke_col);
+    return $svg_obj;
+}
+
+#--- output SVG file ---
+sub output_SVG_file{
+    # options
+    shift if (@_ && $_[0] =~ /$MODULE_NAME/);
+    my %parm = @_;
+    my $svg_obj = $parm{svg_obj};
+    my $svg_file = $parm{svg_file};
+
+    open (SVGFILE,">$svg_file") || die"fail write SVG file: $!\n";
+    print SVGFILE $svg_obj->xmlify;
+    close SVGFILE;
+}
+
+#--- 
+1; ## tell the perl script the successful access of this module.
