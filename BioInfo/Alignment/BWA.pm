b9f81dc34cfd2b8ccf4b90a38de9ca2fd2c16166
Nobel-Justin <wenlongkxm@gmail.com>
2019-06-04 12:49:09 +0800

    apply fq/bam objects

--- BioInfo/Alignment/BWA.pm
+++ BioInfo/Alignment/BWA.pm
@@ -39,15 +39,15 @@ sub PEfqToSortBam{
     shift if (@_ && $_[0] =~ /$MODULE_NAME/);
     my %parm = @_;
     my $ref = $parm{ref};
-    my $fqPathAf = $parm{fqPathAf};
-    my $bamPath = $parm{bamPath};
+    my $fqAf = $parm{fqAf}; # BioFuse::BioInfo::Objects::SeqData::FastQ_OB
+    my $bam = $parm{bam}; # BioFuse::BioInfo::Objects::SeqData::Bam_OB
     my $bwa = $parm{bwa};
     my $samtools = $parm{samtools};
     # additional
     my $func = $parm{func} || 'mem'; # mem: >70bp, aln: <=70bp
     my $maxIns = $parm{maxIns} || 500; # for 'aln' func, '-a' para
-    my $RG_para = $parm{RG_para}; # '-R xxx' / '-r xxx'
-    my $view_para = $parm{view_para}; # '-F xxx -f xxx'
+    my $RG_para = $parm{RG_para} || ''; # '-R xxx' / '-r xxx'
+    my $view_para = $parm{view_para} || ''; # '-F xxx -f xxx'
     my $discBuPair = $parm{discBuPair} || 0; # discard both unmaped PE-reads
     my $sort_mode = lc($parm{sort_mode} || 'c'); # [c]oordinates or reads-[n]ame
     my $onlyRawBam = $parm{onlyRawBam} || 0; # only get raw bam
@@ -57,6 +57,8 @@ sub PEfqToSortBam{
         cluck_and_exit "<ERROR>\tcannot find $key.\n" unless file_exist(filePath => $parm{$key});
     }
 
+    my $bamPath = $bam->filepath;
+    my @fqPath = map {$_->filepath} @$fqAf;
     # PEfqToInitBam
     my $raw_bamPath = $bamPath.'.init.bam';
     my $raw_bam = BioFuse::BioInfo::Objects::SeqData::Bam_OB->new(filepath => $raw_bamPath, tag => 'rawBam');
@@ -75,18 +77,18 @@ sub PEfqToSortBam{
     my $cmd;
     my $awk = "awk '\$0~/^\@/ || (!(and(\$2,0x4) && and(\$2,0x8)) && xor(and(\$2,0x40), and(\$2,0x80)) && and(\$2,0x1))'";
     if($func eq 'mem'){
-        $cmd =  "$bwa mem -t 8 -Y $RG_para"
-               .(@$fqPathAf == 1 ? '-p ' : '')
-               ."$ref @$fqPathAf 2>/dev/null "
+        $cmd =  "$bwa mem -t 8 -Y $RG_para "
+               .(@fqPath == 1 ? '-p ' : '')
+               ."$ref @fqPath 2>/dev/null "
                .($discBuPair ? "| $awk " : '')
                ."| $samtools view -b -S $view_para -T $ref -o $raw_bamPath - 2>/dev/null";
     }
     else{
         my $sai_1 = $bamPath.'.e1.sai';
         my $sai_2 = $bamPath.'.e2.sai';
-        $cmd =  "($bwa aln -L -m 800000 -l 27 -i 10 -k 3 -t 8 -e 10 -f $sai_1 $ref $fqPathAf->[0] 2>/dev/null) && "
-               ."($bwa aln -L -m 800000 -l 27 -i 10 -k 3 -t 8 -e 10 -f $sai_2 $ref $fqPathAf->[1] 2>/dev/null) && "
-               ."($bwa sampe -a $maxIns $RG_para $ref $sai_1 $sai_2 $fqPathAf->[0] $fqPathAf->[1] 2>/dev/null "
+        $cmd =  "($bwa aln -L -m 800000 -l 27 -i 10 -k 3 -t 8 -e 10 -f $sai_1 $ref $fqPath[0] 2>/dev/null) && "
+               ."($bwa aln -L -m 800000 -l 27 -i 10 -k 3 -t 8 -e 10 -f $sai_2 $ref $fqPath[1] 2>/dev/null) && "
+               ."($bwa sampe -a $maxIns $RG_para $ref $sai_1 $sai_2 $fqPath[0] $fqPath[1] 2>/dev/null "
                .($discBuPair ? "| $awk " : '')
                ."| $samtools view -b -S $view_para -T $ref -o $raw_bamPath - 2>/dev/null) && "
                ."(rm -f $sai_1 $sai_2)";
@@ -95,7 +97,7 @@ sub PEfqToSortBam{
     # inform
     stout_and_sterr "[INFO]\tBWA $func finished.\n"
                          ."\tRef: $ref\n";
-    stout_and_sterr       "\tFq:  $_\n" for @$fqPathAf;
+    stout_and_sterr       "\tFq:  $_\n" for @fqPath;
 
     # only want raw bam
     if($onlyRawBam){
