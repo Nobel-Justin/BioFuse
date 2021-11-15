2058f8a30dfc5c9943fb7bb87197ace69af18900
Nobel-Justin <wenlongkxm@gmail.com>
2021-11-15 20:58:04 +0800

    use info from bam stats

--- BioInfo/Alignment/BamQC.pm
+++ BioInfo/Alignment/BamQC.pm
@@ -5,10 +5,11 @@ use warnings;
 use Getopt::Long;
 use Data::Dumper;
 use Cwd qw/ abs_path /;
+use List::Util qw/ sum /;
 use BioFuse::LoadOn;
 use BioFuse::Util::Log qw/ warn_and_exit stout_and_sterr /;
 use BioFuse::Util::Sys qw/ file_exist /;
-use BioFuse::Util::GZfile qw/ Try_GZ_Write /;
+use BioFuse::Util::GZfile qw/ Try_GZ_Write Try_GZ_Read /;
 use BioFuse::BioInfo::Objects::SeqData::Bam_OB;
 use BioFuse::BioInfo::Objects::SeqData::Reads_OB;
 use BioFuse::BioInfo::Objects::Region::BED_OB;
@@ -28,8 +29,8 @@ my ($VERSION, $DATE, $AUTHOR, $EMAIL, $MODULE_NAME);
 
 $MODULE_NAME = 'BioFuse::BioInfo::Alignment::BamQC';
 #----- version --------
-$VERSION = "0.02";
-$DATE = '2020-02-09';
+$VERSION = "0.03";
+$DATE = '2020-11-15';
 
 #----- author -----
 $AUTHOR = 'Wenlong Jia';
@@ -58,6 +59,7 @@ sub return_HELP_INFO{
          -o  [s]  output folder. <required>
          -i  [s]  sample ID. <required>
          -s  [s]  samtools path. <required>
+         -bq [s]  accumulated base quality to stat. ['20,30']
          -t  [s]  target region BED file, can be used multiple times. <required>
                   1) Format: -t 'key1:xxx.bed' -t 'key2:yyy.bed'
                   2) Please use the non-N region BED file for WGS, see func 'get_nonN'.
@@ -95,6 +97,7 @@ sub Load_moduleVar_to_pubVarPool{
             [ samtools => undef ],
             # option
             [ accuDepth => '4,10,20' ],
+            [ accuBaseQ => '20,30' ],
             [ depthMinBQ => 0 ],
             [ depthMinMQ => 0 ],
             [ depthMaxDP => 8000 ],
@@ -103,6 +106,7 @@ sub Load_moduleVar_to_pubVarPool{
             [ bed => {} ],
             [ QC => {} ], # result
             [ accuDepAf => [] ],
+            [ accuBqAf => [] ],
             # list to abs-path
             [ ToAbsPath_Aref => [ ['bam_path'],
                                   ['output_dir'],
@@ -123,6 +127,7 @@ sub Get_Cmd_Options{
         "-s:s"  => \$V_Href->{samtools},
         # option
         "-d:s"  => \$V_Href->{accuDepth},
+        "-bq:s" => \$V_Href->{accuBaseQ},
         "-db:s" => \$V_Href->{depthMinBQ},
         "-dm:s" => \$V_Href->{depthMinMQ},
         "-dd:s" => \$V_Href->{depthMaxDP},
@@ -140,7 +145,7 @@ sub para_alert{
              || !file_exist(filePath=>$V_Href->{output_dir})
              || !defined $V_Href->{sampleID}
              || !defined $V_Href->{samtools}
-             || scalar @{$V_Href->{target_bed}} == 0
+             # || scalar @{$V_Href->{target_bed}} == 0
             );
 }
 
@@ -148,6 +153,9 @@ sub para_alert{
 sub BamQC{
     # prepare
     &prepare;
+
+    # bam stats
+    &bamStats;
 
     # QC Index
     &calcIndex;
@@ -161,6 +169,8 @@ sub prepare{
     # bam
     $V_Href->{bam} = BioFuse::BioInfo::Objects::SeqData::Bam_OB->new(filepath => $V_Href->{bam_path}, tag => 'toQC');
     $V_Href->{bam}->addTool(samtools => $V_Href->{samtools});
+    # accumulate base quality
+    $V_Href->{accuBqAf} = [split /,/, $V_Href->{accuBaseQ}];
     # bed files
     for my $bedOpt (@{$V_Href->{target_bed}}){
         my ($key, $bed) = (split /:/, $bedOpt);
@@ -176,6 +186,51 @@ sub prepare{
     $V_Href->{accuDepAf} = [split /,/, $V_Href->{accuDepth}];
     # inform
     stout_and_sterr "[INFO]\tprepare well.\n";
+}
+
+#--- bam stats ----
+sub bamStats{
+    # options
+    shift if (@_ && $_[0] =~ /$MODULE_NAME/);
+    my %parm = @_;
+    my $key = $parm{key} || 'total';
+
+    my $statsFile = File::Spec->catfile($V_Href->{output_dir}, "$V_Href->{sampleID}.bam.stats");
+    $V_Href->{bam}->get_bam_stats(statsFile => $statsFile);
+
+    my %baseQual;
+    open (STATS, Try_GZ_Read($statsFile)) || die "fail read bam stats file: $!\n";
+    while(<STATS>){
+        # SN      1st fragments:  9351069
+        if(/^SN\s+1st fragments:\s+(\d+)/){
+            $V_Href->{QC}->{$key}->{Frags_R1} = $1;
+        }
+        # SN      last fragments: 9351069
+        elsif(/^SN\s+last fragments:\s+(\d+)/){
+            $V_Href->{QC}->{$key}->{Frags_R2} = $1;
+        }
+        # SN      insert size average:    177.1
+        elsif(/^SN\s+insert size average:\s+([\d\.]+)/){
+            $V_Href->{QC}->{$key}->{InsertSize} = $1;
+        }
+        # FFQ/LFQ
+        elsif(/^[FL]FQ\s/){
+            my @info = split;
+            $baseQual{$info[0]}{$_-1} += $info[$_] for 2 .. $#info;
+        }
+    }
+    close STATS;
+
+    # accumulate base quality
+    for my $rkey (keys %baseQual){
+        my $bqHf = $baseQual{$rkey};
+        my $rNO = $rkey eq 'FFQ' ? 'R1' : 'R2';
+        my $sumAll = sum(values %$bqHf);
+        for my $bqTh (@{$V_Href->{accuBqAf}}){
+            my $sumOK = sum(map {$bqHf->{$_}} grep $_>=$bqTh, keys %$bqHf);
+            $V_Href->{QC}->{$key}->{"BQ_$rNO"}->{$bqTh} = sprintf "%.2f%%", 100 * $sumOK / $sumAll;
+        }
+    }
 }
 
 #--- QC Index on total level ---
@@ -201,8 +256,8 @@ sub calcIndexFromBam{
         my $reads = BioFuse::BioInfo::Objects::SeqData::Reads_OB->new(ReadsLineText => $_, _rc_optfd => 1);
         # clean reads, except of supplementary alignments
         unless($reads->is_suppmap){
-            $V_Href->{QC}->{$key}->{CleanReads}++;
-            $V_Href->{QC}->{$key}->{CleanBases} += $reads->rlen;
+            $V_Href->{QC}->{$key}->{Reads}++;
+            $V_Href->{QC}->{$key}->{Bases} += $reads->rlen;
         }
         # aligned bases
         next if $reads->is_unmap;
@@ -222,7 +277,7 @@ sub calcIndexFromBam{
     }
     close $fh;
     # ratios
-    $V_Href->{QC}->{$key}->{AlignRate} = sprintf "%.2f%%", 100 * $V_Href->{QC}->{$key}->{AlignReads} / $V_Href->{QC}->{total}->{CleanReads};
+    $V_Href->{QC}->{$key}->{AlignRate} = sprintf "%.2f%%", 100 * $V_Href->{QC}->{$key}->{AlignReads} / $V_Href->{QC}->{total}->{Reads};
     $V_Href->{QC}->{$key}->{UniqRate}  = sprintf "%.2f%%", 100 * $V_Href->{QC}->{$key}->{UniqReads}  / $V_Href->{QC}->{$key}->{AlignReads};
     $V_Href->{QC}->{$key}->{DupRate}   = sprintf "%.2f%%", 100 * $V_Href->{QC}->{$key}->{DupReads}   / $V_Href->{QC}->{$key}->{AlignReads};
     $V_Href->{QC}->{$key}->{MismatchRate} = sprintf "%.2f%%", 100 * $V_Href->{QC}->{$key}->{MismatchBases} / $V_Href->{QC}->{$key}->{UniqBases};
@@ -245,8 +300,13 @@ sub calcIndexFromBam{
 
 #--- write QC report ---
 sub report{
-    my @total_idx = qw/ CleanReads
-                        CleanBases /;
+    my @total_idx = qw/ Reads
+                        Bases
+                        Frags_R1
+                        Frags_R2
+                        InsertSize
+                        BQ_R1
+                        BQ_R2      /;
     my @share_idx = qw/ AlignReads
                         AlignRate
                         AlignBases
@@ -264,7 +324,7 @@ sub report{
     my $report = File::Spec->catfile($V_Href->{output_dir}, "$V_Href->{sampleID}.BamQC.report");
     open (REPORT, Try_GZ_Write($report)) || die "fail write output report file: $!\n";
     print REPORT '##'.`date`;
-    print REPORT "##AlignRate(xxx)=AlignReads(xxx)/CleanReads(total)\n";
+    print REPORT "##AlignRate(xxx)=AlignReads(xxx)/Reads(total)\n";
     print REPORT "##UniqRate(xxx)=UniqReads(xxx)/AlignReads(xxx)\n";
     print REPORT "##DupRate(xxx)=DupReads(xxx)/AlignReads(xxx)\n";
     print REPORT "##MismatchRate(xxx)=MismatchBases(xxx)/UniqBases(xxx)\n";
@@ -278,11 +338,14 @@ sub report{
         print REPORT "RegionSize($key)\t".$V_Href->{bed}->{$key}->length."\n" if $key ne 'total';
         my $keyQC_hf = $V_Href->{QC}->{$key};
         for my $idx (@index){
-            if($idx ne 'Coverage'){
-                print REPORT "$idx($key)\t$keyQC_hf->{$idx}\n";
+            if($idx eq 'Coverage'){ # accumulate depth
+                print REPORT "$idx(>=${_}X,$key)\t$keyQC_hf->{$idx}->{$_}\n" for sort {$a<=>$b} keys %{$keyQC_hf->{$idx}};
+            }
+            elsif($idx =~ /BQ_R[12]/){ # accumulate base quality
+                print REPORT "$idx(>=$_,$key)\t$keyQC_hf->{$idx}->{$_}\n" for sort {$a<=>$b} keys %{$keyQC_hf->{$idx}};
             }
             else{
-                print REPORT "$idx(>=${_}X,$key)\t$keyQC_hf->{$idx}->{$_}\n" for sort {$a<=>$b} keys %{$keyQC_hf->{$idx}};
+                print REPORT "$idx($key)\t$keyQC_hf->{$idx}\n";
             }
         }
     }
